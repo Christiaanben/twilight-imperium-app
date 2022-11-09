03a85bfcc45573b78057ab84b04c07ad46b1688e
Bennie <bennie@comparisure.co.za>
2022-11-10 01:07:13 +0200

    add hex draw methods

--- src/views/board/BoardView.vue
+++ src/views/board/BoardView.vue
@@ -2,49 +2,48 @@
   <h1>Game Board</h1>
   <canvas id="canvas"></canvas>
   <div>
-    <button @click="drawRect">Add Rect</button>
-    <button @click="subWidth">-</button>
-    <button @click="addWidth">+</button>
+    <button @click="drawHex">HEX</button>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import {drawHexFromAxial} from "../../services/board";
 
 export default defineComponent({
   name: "BoardView",
   mounted() {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement
     this.canvas = canvas.getContext("2d")
-    this.drawRect()
   },
   data: () => ({
-    rectWidth: 50,
     canvas: null as CanvasRenderingContext2D|null,
   }),
   methods: {
-    drawRect() {
-      if(this.canvas===null)return
-      // clear canvas
-      this.canvas.clearRect(0, 0, 400, 200);
+    drawHex() {
+      if(this.canvas) {
+        const size = 25
+        drawHexFromAxial(0, 0, size, this.canvas)
 
-      // draw rect
-      this.canvas.beginPath();
-      this.canvas.rect(20, 20, this.rectWidth, 100);
-      this.canvas.stroke();
+        drawHexFromAxial(0, 1, size, this.canvas)
+        drawHexFromAxial(1, 0, size, this.canvas)
+        drawHexFromAxial(1, -1, size, this.canvas)
+        drawHexFromAxial(0, -1, size, this.canvas)
+        drawHexFromAxial(-1, 0, size, this.canvas)
+        drawHexFromAxial(-1, 1, size, this.canvas)
+
+        drawHexFromAxial(2, 0, size, this.canvas)
+        drawHexFromAxial(2, -2, size, this.canvas)
+        drawHexFromAxial(0, -2, size, this.canvas)
+      }
     },
-    addWidth() {
-      this.rectWidth += 10
-      this.drawRect()
-    },
-    subWidth() {
-      this.rectWidth -= 10
-      this.drawRect()
-    }
   }
 })
 </script>
 
 <style scoped>
-
+#canvas{
+  height: 300px;
+  width: 300px;
+}
 </style>
