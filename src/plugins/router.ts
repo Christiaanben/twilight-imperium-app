f0b9926299066a6095a8449b77b43f203a33d8c9
Bennie <bennie@comparisure.co.za>
2022-11-10 23:22:12 +0200

    add prettier to project

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,17 +1,17 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            component: () => import('@/components/HelloWorld.vue'),
-        },
-        {
-            path: '/board',
-            component: () => import('@/views/board/BoardView.vue'),
-        }
-    ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/components/HelloWorld.vue'),
+    },
+    {
+      path: '/board',
+      component: () => import('@/views/board/BoardView.vue'),
+    },
+  ],
 })
 
 export default router
--- src/views/board/BoardView.vue
+++ src/views/board/BoardView.vue
@@ -4,60 +4,60 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
-import {Application, Assets, Sprite} from "pixi.js";
+import { defineComponent } from 'vue'
+import { Application, Assets, Sprite } from 'pixi.js'
 
 export default defineComponent({
-  name: "BoardView",
+  name: 'BoardView',
   mounted() {
     const app = new Application()
     console.log(typeof this.$refs.pixi)
     this.$refs.pixi?.appendChild(app.view)
-    Assets.load('/concrete.png').then(texture => {
+    Assets.load('/concrete.png').then((texture) => {
       const sprite = new Sprite(texture)
       sprite.height /= 6
       sprite.width /= 6
-      const size = sprite.width/2
+      const size = sprite.width / 2
 
-      let q = 0, r = 0
-      sprite.x = app.renderer.width / 2 + (q * Math.sqrt(3) + r * Math.sqrt(3) / 2)*size;
-      sprite.y = app.renderer.height / 2 + (r * 3. / 2)*size;
+      let q = 0,
+        r = 0
+      sprite.x = app.renderer.width / 2 + (q * Math.sqrt(3) + (r * Math.sqrt(3)) / 2) * size
+      sprite.y = app.renderer.height / 2 + ((r * 3) / 2) * size
       console.log('sprite 1', sprite.x, sprite.y)
-      sprite.rotation += Math.PI/2;
+      sprite.rotation += Math.PI / 2
 
       // Rotate around the center
-      sprite.anchor.x = 0.5;
-      sprite.anchor.y = 0.5;
+      sprite.anchor.x = 0.5
+      sprite.anchor.y = 0.5
 
       // Add the sprite to the scene we are building
-      app.stage.addChild(sprite);
+      app.stage.addChild(sprite)
 
       const sprite2 = new Sprite(texture)
-      q=1;r=0
+      q = 1
+      r = 0
       sprite2.height /= 6
       sprite2.width /= 6
-      sprite2.x = app.renderer.width / 2 + (q * Math.sqrt(3) + r * Math.sqrt(3) / 2)*size;
-      sprite2.y =app.renderer.height / 2 + (r * 3. / 2)*size;
+      sprite2.x = app.renderer.width / 2 + (q * Math.sqrt(3) + (r * Math.sqrt(3)) / 2) * size
+      sprite2.y = app.renderer.height / 2 + ((r * 3) / 2) * size
       console.log('sprite 2', sprite2.x, sprite2.y)
 
       // Rotate around the center
-      sprite2.anchor.x = 0.5;
-      sprite2.anchor.y = 0.5;
+      sprite2.anchor.x = 0.5
+      sprite2.anchor.y = 0.5
 
       // Add the sprite to the scene we are building
-      app.stage.addChild(sprite2);
-      sprite2.rotation += Math.PI/2;
+      app.stage.addChild(sprite2)
+      sprite2.rotation += Math.PI / 2
     })
   },
   data: () => ({
     canvas: null as CanvasRenderingContext2D | null,
   }),
   methods: {
-    drawHex() {
-    },
-  }
+    drawHex() {},
+  },
 })
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
