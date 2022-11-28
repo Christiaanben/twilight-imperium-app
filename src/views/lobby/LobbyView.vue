224f011e973e60a57c4f1804d3280ba66c1fef2a
Bennie <bennie@comparisure.co.za>
2022-11-29 00:43:33 +0200

    added color select to LobbyView.vue

--- src/views/lobby/LobbyView.vue
+++ src/views/lobby/LobbyView.vue
@@ -2,8 +2,11 @@
   <h4>Lobby ID: {{ gameStore.lobbyId }}</h4>
   <v-list>
     <v-list-item>
-      You
-      <v-select :items="['Lions', 'Bugs', 'Fish']" />
+      <div class="ma-2">You</div>
+      <v-row>
+        <v-col cols="8"> <v-select label="Faction" :items="['Lions', 'Bugs', 'Fish', 'Humans']" /></v-col
+        ><v-col cols="4"> <v-select label="Color" :items="['red', 'yellow', 'green']" /></v-col>
+      </v-row>
     </v-list-item>
     <v-list-item> - </v-list-item>
     <v-list-item> - </v-list-item>
