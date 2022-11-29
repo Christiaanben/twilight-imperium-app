ad006ef0fc6b7803bfc55f17c301a5b1122454f9
Bennie <bennie@comparisure.co.za>
2022-11-30 00:53:42 +0200

    reduce empty slots as more players join

--- src/views/lobby/LobbyView.vue
+++ src/views/lobby/LobbyView.vue
@@ -2,15 +2,13 @@
   <h4>Lobby ID: {{ gameStore.lobbyId }}</h4>
   <v-list>
     <v-list-item v-for="player in gameStore.lobby?.players">
-      <div class="ma-2">You</div>
+      <div class="ma-2">You {{ player }}</div>
       <v-row>
         <v-col cols="8"> <v-select label="Faction" :items="['Lions', 'Bugs', 'Fish', 'Humans']" /></v-col
         ><v-col cols="4"> <v-select label="Color" :items="['red', 'yellow', 'green']" /></v-col>
       </v-row>
     </v-list-item>
-    <v-list-item> - </v-list-item>
-    <v-list-item> - </v-list-item>
-    <v-list-item> - </v-list-item>
+    <v-list-item v-for="_ in MAX_PLAYER - (gameStore.lobby?.players || []).length"> - </v-list-item>
   </v-list>
   <v-switch label="Ready" v-model="isReady" />
 </template>
@@ -29,6 +27,7 @@ export default defineComponent({
   },
   data: () => ({
     isReady: false,
+    MAX_PLAYER: 6,
   }),
   mounted() {
     this.gameStore.connectWS()
