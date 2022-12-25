71fac6ebfba293b50250494bcf9be51d9db1b9fb
cleverNathalia <38295992+cleverNathalia@users.noreply.github.com>
2022-12-25 22:59:29 +0200

    Changed navbar style, fixed mdi fonts, added twilight imperium font, added space images (#2)

--- src/components/layout/TheNavigationDrawer.vue
+++ src/components/layout/TheNavigationDrawer.vue
@@ -1,25 +1,31 @@
 <template>
-  <v-navigation-drawer width="120">
-    <v-list-item to="/"><v-img src="/icon.jpg" width="48" /></v-list-item>
-    <v-list-item to="/setup"> Setup</v-list-item>
-    <v-list-item to="/board/123"> Board</v-list-item>
-    <v-list-item
-      href="https://images-cdn.fantasyflightgames.com/filer_public/f3/c6/f3c66512-8e19-4f30-a0d4-d7d75701fd37/ti-k0289_learn_to_playcompressed.pdf"
-    >
-      Learn
-    </v-list-item>
-    <v-list-item href="https://youtube.com"> Watch</v-list-item>
-    <v-list-item href="https://discord.com/"> Social</v-list-item>
-    <v-list-item>
-      <v-btn>Sign Up</v-btn>
-    </v-list-item>
-    <v-list-item>
-      <sign-in-dialog />
-    </v-list-item>
+  <v-navigation-drawer app v-model="drawer" class="pa-4" expand-on-hover image="/img/space.webp" permanent theme="dark">
+    <v-list>
+      <v-list-item><v-btn color="primary">Log In</v-btn></v-list-item>
+      <v-list-item>
+        <sign-in-dialog />
+      </v-list-item>
+      <v-list-item prepend-icon="mdi-pencil" to="/setup">Create Lobby</v-list-item>
+      <v-list-item prepend-icon="mdi-view-dashboard" to="/board/123">Board</v-list-item>
+      <v-list-item
+        prepend-icon="mdi-school"
+        href="https://images-cdn.fantasyflightgames.com/filer_public/f3/c6/f3c66512-8e19-4f30-a0d4-d7d75701fd37/ti-k0289_learn_to_playcompressed.pdf"
+      >
+        Learn
+      </v-list-item>
+      <v-list-item prepend-icon="mdi-television-classic" href="https://youtube.com"> Watch</v-list-item>
+      <v-list-item prepend-icon="mdi-chat" href="https://discord.com/"> Social</v-list-item>
+    </v-list>
     <template v-slot:append>
-      <v-list-item>English</v-list-item>
-      <v-list-item>Dark UI</v-list-item>
-      <v-list-item>Help</v-list-item>
+      <v-divider></v-divider>
+      <v-list-item class="justify-center pt-3">
+        <v-btn variant="outlined">Sign Up</v-btn>
+      </v-list-item>
+      <v-list-item class="justify-center">English</v-list-item>
+      <v-list-item class="justify-center">Help</v-list-item>
+      <v-list-item class="justify-center"
+        ><v-switch v-model="isDark" :label="isDark ? 'Light' : 'Dark'" color="primary"></v-switch
+      ></v-list-item>
     </template>
   </v-navigation-drawer>
 </template>
@@ -28,6 +34,10 @@
 import SignInDialog from '../dialogs/SignInDialog.vue'
 export default {
   name: 'TheNavigationDrawer',
+  data: () => ({
+    isDark: false,
+    drawer: true,
+  }),
   components: { SignInDialog },
 }
 </script>
