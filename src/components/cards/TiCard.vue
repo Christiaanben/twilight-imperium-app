a1a0cdbef9875c494470c52970ffee418470ad05
Bennie <bennie@hi.guru>
2022-12-31 12:43:52 +0200

    add card templates and do ti-card format (WIP)

--- src/components/cards/TiCard.vue
+++ src/components/cards/TiCard.vue
@@ -1,17 +1,21 @@
 <template>
-  <v-card style="background-color: gray" height="300" class="mx-auto rounded-lg" width="197">
-    <v-img src="/img/cards/action-card-template.webp">
-      <div class="pa-2">
-        <v-card-title class="text-yellow text-uppercase py-4 px-2"> Sabotage </v-card-title>
-        <v-card-text class="text-white text-body-2">
+  <v-card style="background-color: gray" height="300" class="mx-auto rounded-lg" width="201">
+    <v-img :src="imageSrc">
+      <div class="d-flex flex-column py-2 px-4">
+        <div class="title px-2" :class="{ action: type === 'action' }" :style="[titleBackground]">
+          {{ title }}
+        </div>
+        <div v-if="subtitle" class="text-white text-center text-uppercase">{{ subtitle }}</div>
+        <div class="text-body-2 pl-3" :class="[['agenda', 'promissory'].includes(type) ? 'text-black' : 'text-white']">
           When another player plays an action card other than "Sabotage":<br />Cancel that action card.
-        </v-card-text>
-        <v-card-text class="text-white">
-          Unlenn smashed a clenched fist into the bulkhead, green eyes ablaze with rage.
-        </v-card-text>
+        </div>
+        <div v-if="flavor" class="text-white text-center align-self-end pa-2 font-weight-thin font-italic">
+          {{ flavor }}
+        </div>
       </div>
     </v-img>
   </v-card>
+  ========================
 </template>
 
 <script lang="ts">
@@ -19,12 +23,68 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'TiCard',
-  data: () => ({
-    IMAGE_PATHS: {
-      action: '/img/cards/action-card-template.webp',
+  props: {
+    type: {
+      type: String,
+      default: '',
     },
-  }),
+    title: {
+      type: String,
+      default: '',
+    },
+    subtitle: {
+      type: String,
+      default: '',
+    },
+    flavor: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({}),
+  computed: {
+    imageSrc(): string {
+      return (
+        {
+          action: '/img/cards/action-card-template.jfif',
+          agenda: '/img/cards/agenda-card-template.jfif',
+          promissory: '/img/cards/promissory-card-template.jfif',
+          secret: '/img/cards/secret-card-template.jfif',
+          stage1: '/img/cards/stage1-card-template.jfif',
+          stage2: '/img/cards/stage2-card-template.jfif',
+        }[this.type] || ''
+      )
+    },
+    titleBackground(): string {
+      const rgb =
+        {
+          action: 'rgb(255,255,0)',
+          agenda: 'rgb(0, 153, 255)',
+          promissory: 'rgb(80, 100, 80)',
+          secret: 'rgb(255, 0, 38)',
+          stage1: 'rgb(250, 166, 49)',
+          stage2: 'rgb(38, 153, 255)',
+        }[this.type] || 'rgb(255, 255, 255)'
+      return `background: -webkit-linear-gradient(left, ${rgb}, rgb(255, 255, 255)); -webkit-background-clip: text;`
+    },
+  },
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.title {
+  text-align: center;
+  font-size: large;
+  line-height: normal;
+  text-transform: uppercase;
+  background-clip: content-box;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.action {
+  text-align: left !important;
+  padding-top: 24px !important;
+  padding-left: 16px !important;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,41 @@
   <h3>Enter game ID</h3>
   <v-text-field label="Lobby ID" v-model="lobbyId" />
   <v-btn @click="connect">Connect</v-btn>
+
+  <v-row>
+    <v-col cols="4">
+      <ti-card
+        type="action"
+        title="Sabotage"
+        flavor="Unlenn smashed a clenched fist into the bulkhead, green eyes ablaze with rage. The entire plan had been turned on its head. Such incompetence was unfathomable."
+      />
+    </v-col>
+    <v-col cols="4">
+      <ti-card type="agenda" title="Conventions of War" subtitle="Law" />
+    </v-col>
+    <v-col cols="4">
+      <ti-card type="promissory" title="Trade Agreement" />
+    </v-col>
+    <v-col cols="4">
+      <ti-card type="secret" title="Unveil Flagship" subtitle="Action Phase" />
+    </v-col>
+    <v-col cols="4">
+      <ti-card type="stage1" title="Expand Borders" subtitle="Status Phase" />
+    </v-col>
+    <v-col cols="4">
+      <ti-card type="stage2" title="Galvanize the People" subtitle="Status Phase" />
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
 import { useLobbyStore } from '../stores/lobby'
 import { defineComponent } from 'vue'
+import TiCard from '../components/cards/TiCard.vue'
 
 export default defineComponent({
   name: 'HomeView',
+  components: { TiCard },
   setup() {
     const lobbyStore = useLobbyStore()
     return {
