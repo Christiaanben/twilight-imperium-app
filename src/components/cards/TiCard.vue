722865c03fdaa46ca8c39116cb1736932267f624
Bennie <bennie@hi.guru>
2023-01-01 17:20:54 +0200

    finish TiCard styling

--- src/components/cards/TiCard.vue
+++ src/components/cards/TiCard.vue
@@ -1,15 +1,27 @@
 <template>
   <v-card style="background-color: gray" height="300" class="mx-auto rounded-lg" width="201">
     <v-img :src="imageSrc">
-      <div class="d-flex flex-column py-2 px-4">
+      <div class="d-flex flex-column py-2 px-4 fill-height">
         <div class="title px-2" :class="{ action: type === 'action' }" :style="[titleBackground]">
           {{ title }}
         </div>
-        <div v-if="subtitle" class="text-white text-center text-uppercase">{{ subtitle }}</div>
-        <div class="text-body-2 pl-3" :class="[['agenda', 'promissory'].includes(type) ? 'text-black' : 'text-white']">
-          When another player plays an action card other than "Sabotage":<br />Cancel that action card.
+        <div
+          v-if="subtitle"
+          class="text-center text-uppercase"
+          :class="[{ 'mt-3': type === 'agenda' }, type === 'agenda' ? 'text-yellow' : 'text-white']"
+        >
+          {{ subtitle }}
         </div>
-        <div v-if="flavor" class="text-white text-center font-italic mx-1 flavor">
+        <div
+          class="text-body-2 pl-2 mt-5"
+          :class="[
+            ['agenda', 'promissory'].includes(type) ? 'text-black' : 'text-white',
+            ['secret', 'stage1', 'stage2'].includes(type) ? 'text-center' : '',
+          ]"
+          v-html="body"
+        />
+        <div v-if="flavor" class="text-white text-center font-italic ma-1 flavor mt-auto">
+          <v-divider color="white" class="ma-2" />
           {{ flavor }}
         </div>
       </div>
@@ -36,6 +48,10 @@ export default defineComponent({
       type: String,
       default: '',
     },
+    body: {
+      type: String,
+      default: '',
+    },
     flavor: {
       type: String,
       default: '',
@@ -55,8 +71,8 @@ export default defineComponent({
         }[this.type] || ''
       )
     },
-    titleBackground(): string {
-      const rgb =
+    rgb(): string {
+      return (
         {
           action: 'rgb(255,255,0)',
           agenda: 'rgb(0, 153, 255)',
@@ -65,7 +81,10 @@ export default defineComponent({
           stage1: 'rgb(250, 166, 49)',
           stage2: 'rgb(38, 153, 255)',
         }[this.type] || 'rgb(255, 255, 255)'
-      return `background: -webkit-linear-gradient(left, ${rgb}, rgb(255, 255, 255)); -webkit-background-clip: text;`
+      )
+    },
+    titleBackground(): string {
+      return `background: -webkit-linear-gradient(left, ${this.rgb}, rgb(255, 255, 255)); -webkit-background-clip: text;`
     },
   },
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,23 +12,48 @@
       <ti-card
         type="action"
         title="Sabotage"
+        body="When another player plays an action card other than 'Sabotage':<br />Cancel that action card."
         flavor="Unlenn smashed a clenched fist into the bulkhead, green eyes ablaze with rage. The entire plan had been turned on its head. Such incompetence was unfathomable."
       />
     </v-col>
     <v-col cols="4">
-      <ti-card type="agenda" title="Conventions of War" subtitle="Law" />
+      <ti-card
+        type="promissory"
+        title="Trade Agreement"
+        body="When the blue player replenishes commodities:<br/>The blue player gives you all of his commodities. <br/>Then, return this card to the blue player."
+      />
     </v-col>
     <v-col cols="4">
-      <ti-card type="promissory" title="Trade Agreement" />
+      <ti-card
+        type="secret"
+        title="Unveil Flagship"
+        subtitle="Action Phase"
+        body="Win a space combat in a system that contains your flagship.<br/> You cannot score this objective if your flagship is destroyed in the combat."
+      />
     </v-col>
     <v-col cols="4">
-      <ti-card type="secret" title="Unveil Flagship" subtitle="Action Phase" />
+      <ti-card
+        type="stage1"
+        title="Expand Borders"
+        subtitle="Status Phase"
+        body="Control 6 planets in non-home systems."
+      />
     </v-col>
     <v-col cols="4">
-      <ti-card type="stage1" title="Expand Borders" subtitle="Status Phase" />
+      <ti-card
+        type="agenda"
+        title="Conventions of War"
+        subtitle="Directive"
+        body="For: Players cannot use BOMBARDMENT against units that are on cultural planets.<br/>Against: Each player that voted 'Against' discards all of his action cards."
+      />
     </v-col>
     <v-col cols="4">
-      <ti-card type="stage2" title="Galvanize the People" subtitle="Status Phase" />
+      <ti-card
+        type="stage2"
+        title="Galvanize the People"
+        subtitle="Status Phase"
+        body="Spend a total of 6 tokens from your tactic and/or strategy pools."
+      />
     </v-col>
   </v-row>
 </template>
