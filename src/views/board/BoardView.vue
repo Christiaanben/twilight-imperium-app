16113adcf720f1281485f82d96b1bcf6650b979c
Bennie the Awesome <bennie@comparisure.co.za>
2023-01-05 09:46:54 +0200

    replace png with webp

--- src/views/board/BoardView.vue
+++ src/views/board/BoardView.vue
@@ -1,35 +1,35 @@
 <template>
+  <v-btn @click="zoom">Zoom</v-btn>
   <div id="pixiCanvas" ref="pixi"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Application, Assets, Sprite } from 'pixi.js'
+import { Assets, Sprite } from 'pixi.js'
 import { hexToPoint } from '../../services/hex-service'
 import { useGameStore } from '../../stores/game'
+import { useGraphicsStore } from '../../stores/graphics'
 
 export default defineComponent({
   name: 'BoardView',
   setup() {
     const gameStore = useGameStore()
+    const graphicsStore = useGraphicsStore()
     return {
       gameStore,
+      graphicsStore,
     }
   },
   async mounted() {
     await this.gameStore.hydrateGame(this.$route.params.id as string)
-    const app = new Application({
-      height: 990,
-      width: 1920,
-      backgroundColor: '0x080810',
-    })
+    const app = this.graphicsStore.app
     const pixiDiv = this.$refs.pixi as HTMLDivElement
     const pixiCanvas = app.view as HTMLCanvasElement
     pixiDiv.appendChild(pixiCanvas)
-    const scaleFactor = 1.8
+    const scaleFactor = 3.6
 
     this.gameStore.systems.forEach((system) => {
-      Assets.load(`/img/systems/${system.id}.png`).then((texture) => {
+      Assets.load(`/img/systems/${system.id}.webp`).then((texture) => {
         const sprite = new Sprite(texture)
         sprite.scale.set(1 / scaleFactor, 1 / scaleFactor)
         const size = sprite.width / 2
@@ -50,7 +50,9 @@ export default defineComponent({
     canvas: null as CanvasRenderingContext2D | null,
   }),
   methods: {
-    drawHex() {},
+    zoom() {
+      this.graphicsStore.app.stage.scale.set(1.2, 1.2)
+    },
   },
 })
 </script>
