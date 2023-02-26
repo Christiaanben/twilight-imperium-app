468954b58f7026ac1088ef56aaa0250f791a746d
Bennie the Awesome <bennie@comparisure.co.za>
2023-02-26 23:43:05 +0200

    autoReconnect websocket

--- src/services/web-service/index.ts
+++ src/services/web-service/index.ts
@@ -5,7 +5,9 @@ import { Strategy } from '../../interfaces/strategy'
 let socket: UseWebSocketReturn<any> | null = null
 
 export async function connectToGame(gameId: string) {
-  socket = useWebSocket(`${import.meta.env.VITE_WEB_WS}/ws/game/${gameId}/?token=${useAuthStore().token}`, {})
+  socket = useWebSocket(`${import.meta.env.VITE_WEB_WS}/ws/game/${gameId}/?token=${useAuthStore().token}`, {
+    autoReconnect: true,
+  })
 }
 
 export function selectStrategy(strategy: Strategy) {
