ce49ac9e10fc2f4d6bd797912d7adfb5b3c3596b
Bennie <bennie@comparisure.co.za>
2023-02-28 00:24:59 +0200

    save and display strategies

--- src/services/web-service/index.ts
+++ src/services/web-service/index.ts
@@ -1,12 +1,26 @@
 import { useWebSocket, UseWebSocketReturn } from '@vueuse/core'
 import { useAuthStore } from '../../stores/auth'
 import { Strategy } from '../../interfaces/strategy'
+import { useGameStore } from '../../stores/game'
 
 let socket: UseWebSocketReturn<any> | null = null
 
 export async function connectToGame(gameId: string) {
   socket = useWebSocket(`${import.meta.env.VITE_WEB_WS}/ws/game/${gameId}/?token=${useAuthStore().token}`, {
     autoReconnect: true,
+    onMessage(ws, event) {
+      const data = JSON.parse(event.data)
+      console.debug(`[websocket][event:${data.type}]`, data)
+      switch (data.type) {
+        case 'select_strategy':
+          let replaced = false
+          useGameStore().strategies.map((strategy) => {
+            if (data.kwargs.type === strategy.type) replaced = true
+            return strategy
+          })
+          if (!replaced) useGameStore().strategies.push(data.kwargs)
+      }
+    },
   })
 }
 
