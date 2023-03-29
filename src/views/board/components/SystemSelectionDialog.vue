c9ccae5c8896a1f98f6eaa9e82af9006a6091649
Bennie <bennie@comparisure.co.za>
2023-03-30 00:26:07 +0200

    add dummy data to SystemSelectionDialog.vue

--- src/views/board/components/SystemSelectionDialog.vue
+++ src/views/board/components/SystemSelectionDialog.vue
@@ -1,7 +1,24 @@
 <template>
-  <v-dialog v-model="showOverlay" max-width="50rem" class="d-flex justify-end ma-16">
-    <v-card>
+  <v-dialog v-model="showOverlay" class="d-flex justify-end ma-16" max-width="40rem">
+    <v-card class="pa-4">
       <v-card-title>System Selection</v-card-title>
+      <v-card-subtitle>
+        <p>System: {{ gameStore.selectedSystemId }}</p>
+        <p>Type: Nebula</p>
+      </v-card-subtitle>
+      <v-card-text>
+        <v-list>
+          <v-list-item-title>SPACE</v-list-item-title>
+          <v-list-item> Carrier (1) </v-list-item>
+          <v-list-item-title>Planet: X</v-list-item-title>
+          <v-list-item> Infantry (3) </v-list-item>
+          <v-list-item-title>Planet: Y</v-list-item-title>
+          <v-list-item> PDS (1) </v-list-item>
+        </v-list>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn block color="primary" variant="elevated">Activate</v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
