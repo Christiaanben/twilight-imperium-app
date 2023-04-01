da4965657073ea53e7871e94247fb7d79a6bd0eb
cleverNathalia <danielledramaqueeeen@gmail.com>
2023-04-01 23:33:07 +0200

    cleaned up the sign in and sign up pages

--- src/views/auth/SignInView.vue
+++ src/views/auth/SignInView.vue
@@ -1,69 +1,87 @@
 <template>
-  <h1 >Twilight Imperium Sign In</h1>
-  <h2>Please add the following details: </h2>
-  <form data-cy="formSignIn">
-    <label data-cy="labelEmail" >Email:</label>
-    <v-text-field data-cy="textEmail" v-model="email" @blur="vuelidate.email.$touch"></v-text-field>
-    <div v-if="vuelidate.email.$error">Please add a valid email.</div><br>
-
-    <label data-cy="labelPassword" >Password:</label>
-    <v-text-field data-cy="textPassword" v-model="password" @blur="vuelidate.password.$touch"></v-text-field>
-    <div v-if="vuelidate.password.$error">Please add a valid password</div><br>
-
-
-    <v-btn data-cy="buttonSubmit" @click="signInAccount" > Submit</v-btn>
-  </form>
+  <v-card
+    class="d-flex justify-center align-center"
+    density="comfortable"
+    height="100%"
+    image="/img/spacetortoise.webp"
+    theme="dark"
+  >
+    <v-sheet class="pa-4 overflow-auto" position="fixed" width="40%" height="40%" rounded color="rgb(0, 0, 0, 0.8)">
+      <h1 class="mt-6">Sign In</h1>
+      <form data-cy="formSignIn">
+        <v-text-field
+          class="py-2"
+          data-cy="textEmail"
+          v-model="email"
+          :rules="[rules.validEmail]"
+          @blur="vuelidate.email.$touch"
+          variant="filled"
+          label="Email"
+        ></v-text-field>
+        <v-text-field
+          class="py-2"
+          data-cy="textPassword"
+          v-model="password"
+          :rules="[rules.validPassword]"
+          @blur="vuelidate.password.$touch"
+          variant="filled"
+          label="Password"
+        ></v-text-field>
+        <v-btn data-cy="buttonSubmit" @click="signInAccount"> Submit</v-btn>
+      </form>
+    </v-sheet>
+  </v-card>
 </template>
 
 <script lang="ts">
-
 import { defineComponent } from 'vue'
-import {useAuthStore} from "../../stores/auth";
+import { useAuthStore } from '../../stores/auth'
 import { useVuelidate } from '@vuelidate/core'
-import { required, email} from '@vuelidate/validators'
+import { required, email } from '@vuelidate/validators'
 
 export default defineComponent({
-  name: "SignInView",
+  name: 'SignInView',
   setup() {
     const authStore = useAuthStore()
     return {
       authStore,
-      vuelidate: useVuelidate()
+      vuelidate: useVuelidate(),
     }
   },
   data: () => ({
     email: '',
     password: '',
+    rules: {
+      validEmail: (value: any) => !!value || 'Please add a valid email.',
+      validPassword: (value: any) => !!value || 'Please add a valid password.',
+    },
   }),
   methods: {
     async signInAccount() {
-      if (await this.vuelidate.$validate()){
+      if (await this.vuelidate.$validate()) {
         //ToDo add loader.
         const registerSuccess = await this.authStore.signIn(this.email, this.password)
         let userSuccess = false
-        if(registerSuccess){
+        if (registerSuccess) {
           userSuccess = await this.authStore.hydrateUser()
         }
         //ToDo disable loader
-        if(registerSuccess && userSuccess){
-          this.$router.push({ path: '/'})
-        }
-        else{
+        if (registerSuccess && userSuccess) {
+          this.$router.push({ path: '/' })
+        } else {
           //ToDO Add error message and response
           console.log(this.authStore.error_message.message)
         }
       }
     },
   },
-  validations () {
+  validations() {
     return {
-      email: { required, email,},
-      password: { required,}
+      email: { required, email },
+      password: { required },
     }
-  }
+  },
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/auth/SignUpView.vue
+++ src/views/auth/SignUpView.vue
@@ -1,76 +1,100 @@
 <template>
-  <h1 >Twilight Imperium Sign Up</h1>
-  <h2>Please add the following details: </h2>
-  <form data-cy="formSignUp">
-    <label data-cy="labelDisplayName">Display Name:</label>
-    <v-text-field data-cy="textFieldDisplayName" v-model="displayName" @blur="vuelidate.displayName.$touch"></v-text-field>
-    <div v-if="vuelidate.displayName.$error">Please add a valid display name.</div><br>
-
-    <label data-cy="labelEmail">Email:</label>
-    <v-text-field data-cy="textEmail" v-model="email" @blur="vuelidate.email.$touch"></v-text-field>
-    <div v-if="vuelidate.email.$error">Please add a valid email.</div><br>
-
-    <label data-cy="labelPassword">Password:</label>
-    <v-text-field data-cy="textPassword" v-model="password" @blur="vuelidate.password.$touch"></v-text-field>
-    <div v-if="vuelidate.password.$error">Please add a valid password</div><br>
-
-
-    <v-btn data-cy="buttonSubmit" @click="registerAccount" > Submit</v-btn>
-  </form>
+  <v-card
+    class="d-flex justify-center align-center"
+    density="comfortable"
+    height="100%"
+    image="/img/spacecats.webp"
+    theme="dark"
+  >
+    <v-sheet class="pa-4 overflow-auto" position="fixed" width="40%" height="50%" rounded color="rgb(0, 0, 0, 0.8)">
+      <h1 class="mt-4">Sign Up</h1>
+      <form data-cy="formSignUp">
+        <v-text-field
+          class="py-2"
+          data-cy="textFieldDisplayName"
+          v-model="displayName"
+          :rules="[rules.validDisplayName]"
+          @blur="vuelidate.displayName.$touch"
+          variant="filled"
+          label="Display Name"
+        ></v-text-field>
+        <v-text-field
+          class="py-2"
+          data-cy="textEmail"
+          v-model="email"
+          :rules="[rules.validEmail]"
+          @blur="vuelidate.email.$touch"
+          variant="filled"
+          label="Email"
+        ></v-text-field>
+        <v-text-field
+          class="py-2"
+          data-cy="textPassword"
+          v-model="password"
+          :rules="[rules.validPassword]"
+          @blur="vuelidate.password.$touch"
+          variant="filled"
+          label="Password"
+        ></v-text-field>
+        <v-btn class="mt-4" data-cy="buttonSubmit" @click="registerAccount"> Submit</v-btn>
+      </form>
+    </v-sheet>
+  </v-card>
 </template>
 
 <script lang="ts">
-
 import { defineComponent } from 'vue'
-import {useAuthStore} from "../../stores/auth";
+import { useAuthStore } from '../../stores/auth'
 import { useVuelidate } from '@vuelidate/core'
-import { required, email} from '@vuelidate/validators'
+import { required, email } from '@vuelidate/validators'
 
 export default defineComponent({
-  name: "SignUpView",
+  name: 'SignUpView',
   setup() {
     const authStore = useAuthStore()
     return {
       authStore,
-      vuelidate: useVuelidate()
+      vuelidate: useVuelidate(),
     }
   },
   data: () => ({
     displayName: '',
     email: '',
     password: '',
+    rules: {
+      validDisplayName: (value: any) => !!value || 'Please add a valid display name.',
+      validEmail: (value: any) => !!value || 'Please add a valid email.',
+      validPassword: (value: any) => !!value || 'Please add a valid password.',
+    },
   }),
   methods: {
     async registerAccount() {
-      if (await this.vuelidate.$validate()){
+      if (await this.vuelidate.$validate()) {
         //ToDo add loader.
         const registerSuccess = await this.authStore.register(this.displayName, this.email, this.password)
         let userSuccess = false
-        if(registerSuccess){
-            userSuccess = await this.authStore.hydrateUser()
+        if (registerSuccess) {
+          userSuccess = await this.authStore.hydrateUser()
         }
 
         //ToDo disable loader
-        if(registerSuccess && userSuccess){
-          this.$router.push({ path: '/'})
-        }
-        else{
+        if (registerSuccess && userSuccess) {
+          this.$router.push({ path: '/' })
+        } else {
           //ToDO Add error message and response
           console.log(this.authStore.error_message.message)
         }
       }
     },
   },
-  validations () {
+  validations() {
     return {
-      displayName: {required},
-      email: {required, email,},
-      password: {required,}
+      displayName: { required },
+      email: { required, email },
+      password: { required },
     }
-  }
+  },
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
